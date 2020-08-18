0653db68df7b06c9f6605ec3b4352d56e5972b46
weiyu <zhouweiyu@meituan.com>
2020-08-18 11:39:41 +0800

    install nvim

--- install.sh
+++ install.sh
@@ -2,6 +2,16 @@
 
 set -eo pipefail
 
+function install_nvim() {
+    wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+    chmod u+x nvim.appimage
+    ./nvim.appimage --appimage-extract
+    curdir=$(pwd)
+    ln -s $curdir/squashfs-root/usr/bin/nvim /usr/bin/nvim
+}
+
+install_nvim
+
 curl -fLo \
      ~/.config/nvim/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
